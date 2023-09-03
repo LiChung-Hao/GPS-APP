6bab89d3ef9a50dcee4b527e109f61a277d65c98
Li, ChungHao <wesley.lee.hau@gmail.com>
2023-09-03 15:41:04 -0400

    structure of the app

---
+++ PlaceFinder/Assets/Pack/Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	public static string keyword;
+	// Use this for initialization
+	void Start () {
+		Debug.Log(keyword);
+	}
+
+	public void AskGoogle(string passedKeyword)
+	{
+        keyword=passedKeyword;
+
+    }
+	public void ChangeScene(string scene)
+	{ 
+		SceneManager.LoadSceneAsync(scene);
+	}
+	public void Quit()
+	{ 
+		Application.Quit();
+	}
+}
